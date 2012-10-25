b8430a9953a77e8568f844aa8dbdf29e57516eed
Gabor Ancsin <ancsingaboristvan@gmail.com>
2012-10-25 14:56:40 +0200

    jQuery plugin wrapper, lowercase functionality for tag names, jslint checked. Not tested :-)

--- dom-o.js
+++ dom-o.js
@@ -1,92 +1,108 @@
-!function() {
-  var root  = this
-  var slice = Array.prototype.slice
-  var has   = Object.prototype.hasOwnProperty
-  var tags  = [
-    "A", "ABBR", "ACRONYM", "ADDRESS", "AREA", "ARTICLE", "ASIDE", "AUDIO",
-    "B", "BDI", "BDO", "BIG", "BLOCKQUOTE", "BODY", "BR", "BUTTON",
-    "CANVAS", "CAPTION", "CITE", "CODE", "COL", "COLGROUP", "COMMAND",
-    "DATALIST", "DD", "DEL", "DETAILS", "DFN", "DIV", "DL", "DT", "EM",
-    "EMBED", "FIELDSET", "FIGCAPTION", "FIGURE", "FOOTER", "FORM", "FRAME",
-    "FRAMESET", "H1", "H2", "H3", "H4", "H5", "H6", "HEAD", "HEADER",
-    "HGROUP", "HR", "HTML", "I", "IFRAME", "IMG", "INPUT", "INS", "KBD",
-    "KEYGEN", "LABEL", "LEGEND", "LI", "LINK", "MAP", "MARK", "META",
-    "METER", "NAV", "NOSCRIPT", "OBJECT", "OL", "OPTGROUP", "OPTION",
-    "OUTPUT", "P", "PARAM", "PRE", "PROGRESS", "Q", "RP", "RT", "RUBY",
-    "SAMP", "SCRIPT", "SECTION", "SELECT", "SMALL", "SOURCE", "SPAN",
-    "SPLIT", "STRONG", "STYLE", "SUB", "SUMMARY", "SUP", "TABLE", "TBODY",
-    "TD", "TEXTAREA", "TFOOT", "TH", "THEAD", "TIME", "TITLE", "TR",
-    "TRACK", "TT", "UL", "VAR", "VIDEO", "WBR"
-  ]
+(function ($) {
+    "use strict";
+    $.fn.dom_o = function () {
+    var root  = this.dom_o,
+        slice = Array.prototype.slice,
+        has   = Object.prototype.hasOwnProperty,
+        lCaseTags = true,
+        tags  = [
+            "A", "ABBR", "ACRONYM", "ADDRESS", "AREA", "ARTICLE", "ASIDE", "AUDIO",
+            "B", "BDI", "BDO", "BIG", "BLOCKQUOTE", "BODY", "BR", "BUTTON",
+            "CANVAS", "CAPTION", "CITE", "CODE", "COL", "COLGROUP", "COMMAND",
+            "DATALIST", "DD", "DEL", "DETAILS", "DFN", "DIV", "DL", "DT", "EM",
+            "EMBED", "FIELDSET", "FIGCAPTION", "FIGURE", "FOOTER", "FORM", "FRAME",
+            "FRAMESET", "H1", "H2", "H3", "H4", "H5", "H6", "HEAD", "HEADER",
+            "HGROUP", "HR", "HTML", "I", "IFRAME", "IMG", "INPUT", "INS", "KBD",
+            "KEYGEN", "LABEL", "LEGEND", "LI", "LINK", "MAP", "MARK", "META",
+            "METER", "NAV", "NOSCRIPT", "OBJECT", "OL", "OPTGROUP", "OPTION",
+            "OUTPUT", "P", "PARAM", "PRE", "PROGRESS", "Q", "RP", "RT", "RUBY",
+            "SAMP", "SCRIPT", "SECTION", "SELECT", "SMALL", "SOURCE", "SPAN",
+            "SPLIT", "STRONG", "STYLE", "SUB", "SUMMARY", "SUP", "TABLE", "TBODY",
+            "TD", "TEXTAREA", "TFOOT", "TH", "THEAD", "TIME", "TITLE", "TR",
+            "TRACK", "TT", "UL", "VAR", "VIDEO", "WBR"
+        ],
 
-  var i = tags.length
+        i = tags.length;
 
-  while (i--) !function(nodeName) {
-    root[nodeName] = function(attributes) {
-      var childNodes = slice.call(arguments, 1)
+    function registerNodeName(nodeName) {
+        var nName = lCaseTags ? nodeName.toLowerCase() : nodeName;
+        root[nName] = function(attributes) {
+            var childNodes = slice.call(arguments, 1);
 
-      if (typeof attributes != "object" || attributes.nodeType) {
-        childNodes.unshift(attributes)
-        attributes = null
-      }
+            if (typeof attributes !== "object" || attributes.nodeType) {
+                childNodes.unshift(attributes);
+                attributes = null;
+            }
 
-      return Element(document, nodeName, attributes, childNodes)
+            return new Element(document, nName, attributes, childNodes);
+        };
     }
-  }(tags[i])
-
-  function hyphenify(text) {
-    return text.replace(/[A-Z]/g, "-$&").toLowerCase()
-  }
-
-  function Element(document, nodeName, attributes, childNodes) {
-    var child, i, el = document.createElement(nodeName)
-
-    for (i in attributes) if (has.call(attributes, i)) {
-      child = document.createAttribute(hyphenify(i))
-      child.nodeValue = attributes[i]
-      el.setAttributeNode(child)
+    
+    function hyphenify(text) {
+        return text.replace(/[A-Z]/g, "-$&").toLowerCase();
     }
+    
+    function Element(document, nodeName, attributes, childNodes) {
+        var child,
+            i,
+            el = document.createElement(nodeName);
 
-    for (i = 0; i < childNodes.length; i++) {
-      child = childNodes[i]
+        for (i in attributes) {
+            if (has.call(attributes, i)) {
+                child = document.createAttribute(hyphenify(i));
+                child.nodeValue = attributes[i];
+                el.setAttributeNode(child);
+            }
+        }
 
-      if (!child || !child.nodeType) child = document.createTextNode(child)
+        for (i = 0; i < childNodes.length; i += 1) {
+            child = childNodes[i];
 
-      el.appendChild(child)
-    }
+            if (!child || !child.nodeType) {
+                child = document.createTextNode(child);
+            }
+            el.appendChild(child);
+        }
 
-    return el
-  }
-
-  root.CSS = function(selector) {
-    var css = selector + "{"
-    var i = 1
-    var l = arguments.length
-    var key
-    var block
-
-    while (i < l) {
-      block = arguments[i++]
-
-      switch (typeof block) {
-        case "object":
-          for (key in block) {
-            css += hyphenify(key) + ":" + block[key]
-
-            if (typeof block[key] == "number") css += "px"
-
-            css += ";"
-          }
-          break
-
-        case "string":
-          css = selector + " " + block + css
-          break
-      }
+        return el;
     }
 
-    css += "}\n"
+    while ((i -= 1))  {
+        registerNodeName(tags[i]);
+    }
 
-    return css
-  }
-}()
+
+
+    root.CSS = function(selector) {
+        var css = selector + "{",
+            i = 1,
+            l = arguments.length,
+            key,
+            block;
+
+        while (i < l) {
+            block = arguments[i += 1];
+
+            switch (typeof block) {
+                case "object":
+                  for (key in block) {
+                      if (block.hasOwnProperty(key)) {
+                          css += hyphenify(key) + ":" + block[key];
+                          if (typeof block[key] === "number") {
+                              css += "px";
+                          }
+                          css += ";";
+                      }
+                  }
+                  break;
+
+                case "string":
+                  css = selector + " " + block + css;
+                  break;
+            }
+        }
+        css += "}\n";
+        return css;
+    };
+};
+}(jQuery));
